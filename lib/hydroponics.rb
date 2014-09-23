56b012cefba0392d8620a099f97f950dc73be0fa
Cameron <cameron.p.buckingham@gmail.com>
2014-09-23 10:06:23 -0600

    first set of averages

---
+++ lib/hydroponics.rb
@@ -0,0 +1,41 @@
+require 'csv'
+
+class Hydroponics
+
+  def initialize(filepath)
+    @filepath = filepath
+  end
+
+
+  def averages(containers)
+    ph_sum, nsl_sum, temp_sum, water_level_sum, count = 0, 0, 0, 0, 0.0
+    CSV.foreach(@filepath, {:col_sep => " "}) do |row|
+      if row[3][-1].to_i == containers
+        ph_sum += row[4].to_f
+        nsl_sum += row[5].to_i
+        temp_sum += row[6].to_i
+        water_level_sum += row[7].to_f
+        count += 1.0
+      end
+    end
+    {
+      container: containers,
+      ph: (ph_sum/count).round(2),
+      nsl: (nsl_sum/count).round(2),
+      temp: (temp_sum/count).round(2),
+      water_level: (water_level_sum/count).round(2)
+    }
+  end
+
+  def highest_averages(containers)
+    temp_sum, water_level_sum, count = 0, 0, 0, 0, 0.0
+    CSV.foreach(@filepath, {:col_sep => " "}) do |row|
+      if row[3][-1].to_i == containers
+        temp_sum += row[6].to_i
+        water_level_sum += row[7].to_f
+        count += 1.0
+      end
+  end
+  end
+end
+
---
+++ spec/hyrdoponics_spec.rb
@@ -0,0 +1,49 @@
+require "spec_helper"
+
+describe "hydroponics" do
+  it "takes a file path as argument" do
+    Hydroponics.new("./data/metrics.tsv")
+  end
+
+  it "returns a hash with averages" do
+    container = Hydroponics.new("./data/metrics.tsv")
+    averages = container.averages(1)
+    expected_output = {
+      container: 1,
+      ph: 5.01,
+      nsl: 39.02,
+      temp: 57.76,
+      water_level: 2.12
+    }
+    {
+      container: 2,
+      ph: 5.95,
+      nsl: 19.77,
+      temp: 73.06,
+      water_level: 3.8
+    }
+    {
+      container: 3,
+      ph: 7.01,
+      nsl: 10.97,
+      temp: 67.61,
+      water_level: 4.7
+    }
+    expect(averages).to eq(expected_output)
+  end
+
+  it "returns highest average temp" do
+    container = Hydroponics.new("./data/metrics.tsv")
+    highest_average = container
+    expected_output = "container2"
+    expect(highest_average).to eq(expected_output)
+  end
+  it "returns highest water level" do
+    container = Hydroponics.new("./data/metrics.tsv")
+    highest_average = container
+    expected_output = "container3"
+    expect(highest_average).to eq(expected_output)
+  end
+end
+end
+
